272f786645e5e05930892ace6879cff27a4d97e8
developer@ser2.autovaruosad.ee <admin@ser2.autovaruosad.ee>
2025-02-20 20:41:01 +0200

    First comit

--- src/module.ts
+++ src/module.ts
@@ -5,8 +5,8 @@ export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule',
+    name: 'nuxt-ow-base',
+    configKey: 'nuxtOwBase',
   },
   // Default configuration options of the Nuxt module
   defaults: {},
