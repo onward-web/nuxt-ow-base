1673d455ec04e6b9571617019fec4259a91d8370
developer@ser2.autovaruosad.ee <admin@ser2.autovaruosad.ee>
2025-02-20 21:40:18 +0200

    First comit

---
+++ dist/module.d.ts
@@ -0,0 +1,2 @@
+export * from "/home/admin/web/autopart.onward-web.pp.ua/public_html/nuxt-ow-base/src/module";
+export { default } from "/home/admin/web/autopart.onward-web.pp.ua/public_html/nuxt-ow-base/src/module";
---
+++ dist/module.mjs
@@ -0,0 +1,12 @@
+import jiti from "file:///home/admin/web/autopart.onward-web.pp.ua/public_html/nuxt-ow-base/node_modules/.pnpm/jiti@1.21.7/node_modules/jiti/lib/index.js";
+
+/** @type {import("/home/admin/web/autopart.onward-web.pp.ua/public_html/nuxt-ow-base/src/module")} */
+const _module = jiti(null, {
+  "esmResolve": true,
+  "interopDefault": true,
+  "alias": {
+    "nuxt-ow-base": "/home/admin/web/autopart.onward-web.pp.ua/public_html/nuxt-ow-base"
+  }
+})("/home/admin/web/autopart.onward-web.pp.ua/public_html/nuxt-ow-base/src/module.ts");
+
+export default _module;
